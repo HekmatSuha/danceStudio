e24db42b5ef87d04b20c6da312ee8babeba25579
HekmatSuha <hekmatsuha353@gmail.com>
2026-01-16 17:45:49 +0600

    eas

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -2,7 +2,13 @@ import { ExpoConfig, ConfigContext } from "expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: "dance-crm",
-  slug: "dance-crm",
-  extra: {},
+  name: "Tance",
+  slug: "tance",
+  extra: {
+    ...config.extra,
+    router: config.extra?.router ?? {},
+    eas: {
+      projectId: "016d04b9-6d02-48c5-9308-c24d85432618",
+    },
+  },
 });
